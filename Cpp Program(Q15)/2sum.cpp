85d091e340b5335a7215e51de4f9d06775f7444e
Avinash Kamble <contact.buildwithak@gmail.com>
2026-08-19 03:05:34 +0000

    new cpp

---
+++ Cpp Program(Q15)/2sum.cpp
@@ -0,0 +1,18 @@
+// 2) Write a C++ program to input two numbers and display their sum.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    sum = a + b;
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
